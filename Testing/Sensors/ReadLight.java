2bfd4c797ef98468d619dc382370e71c2aef8150
Oliver Tse <olivertsesk@hotmail.com>
2016-04-25 20:19:28 -0400

    Added files via upload

---
+++ Testing/Sensors/ReadLight.java
@@ -0,0 +1,25 @@
+import lejos.robotics.SampleProvider;
+
+
+public class ReadLight {
+
+	private SampleProvider colorSensor;
+	private float[] colorData;
+	
+	public ReadLight(SampleProvider colorSensor, float[] colorData){
+		this.colorSensor = colorSensor;
+		this.colorData = colorData;
+	}
+	
+	public void readSensor() {
+		while(true){
+			System.out.println("COLOR: " + getFilteredData());
+		}
+	}
+	
+	private float getFilteredData(){
+		colorSensor.fetchSample(colorData, 0);
+		float distance = colorData[0];
+		return distance;
+	}
+}
---
+++ Testing/Sensors/ReadSensorTest.java
@@ -0,0 +1,29 @@
+import lejos.hardware.Sound;
+import lejos.hardware.lcd.LCD;
+import lejos.robotics.SampleProvider;
+
+public class ReadSensorTest {
+
+	private SampleProvider usSensor;
+	private float[] usData;
+	private int distance;
+	
+	public ReadSensorTest(SampleProvider usSensor, float[] usData) {
+		this.usSensor = usSensor;
+		this.usData = usData;
+	}
+	
+	public void readSensor() {
+		while(true){
+			System.out.println("DISTANCE:      " + getFilteredData());
+		}
+	}
+
+	private float getFilteredData() {
+		usSensor.fetchSample(usData, 0);
+		float distance = usData[0]*100;
+		if(distance > 90)
+			distance = 90;
+		return distance;
+	}
+}
